4b1e8e3b27de855fc06a5dc5f5f4ef017200932c
Wildson Seguchi <wseguchi@gmail.com>
2023-03-20 23:18:02 -0300

    JS v2a

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 const animais = document.getElementById('animais');
 console.log(animais.getAttribute('id'));
 
-animais.setAttribute('data-test', 'info');
+animais.setAttribute('data-test', 'you can dance');
