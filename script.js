77d4385ef6af5f1bd7d3e8262cbef99d4fc02b10
Wildson Seguchi <wseguchi@gmail.com>
2023-03-25 23:25:52 -0300

    JS v2d

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 const animais = document.getElementById('animais');
 console.log(animais.getAttribute('id'));
 
-animais.setAttribute('data-test', 'having ther time of your life');
+animais.setAttribute('data-test', 'having the time');
