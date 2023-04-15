e1fe9cfbeafd538751528e01a518ac40e48296ef
Wildson Seguchi <wseguchi@gmail.com>
2023-04-14 23:06:14 -0300

    JS v2e

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 const animais = document.getElementById('animais');
 console.log(animais.getAttribute('id'));
 
-animais.setAttribute('data-test', 'having the time lost in my word');
+animais.setAttribute('data-test', 'having the time lost in my words');
